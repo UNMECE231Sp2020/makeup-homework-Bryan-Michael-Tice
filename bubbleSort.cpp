cd2c8b1e5f629dcfe9d322d968d3f4663fa968ee
Francisco Viramontes <kuolemajarvi@yahoo.com>
2020-04-19 13:47:40 -0600

    Added hints on how to move forward with bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -13,12 +13,18 @@ void bubbleSort(int *begin, const int *end){
 	int *tempHead;
 	int *nextoTempHead;
 
+	//Something is funky with your ending condition, I recommend
+	// using the != operator for you ending condition.
 	for(tempHead=begin;tempHead<(end-1);tempHead++){
 		
+		//The starting condition for this for loop should be the
+		// same as the previous for loop
 		for(nextoTempHead=(tempHead+1); nextoTempHead<end; nextoTempHead++) {
 		
+			//This looks good
 			if(*(nextoTempHead)>*(nextoTempHead+1)){
 			
+				//This looks good
 				swap(nextoTempHead,(nextoTempHead+1));
 
 			}
